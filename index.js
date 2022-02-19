5289d738de931dcdb274c2934608df4c639b08ad
Kaique Andrade <kaique_andrade17@hotmail.com>
2022-02-19 12:28:43 -0300

    retomar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,19 @@
+var idade = 18
+console.log("Oi, " + nome)
+if (idade >= 18){
+    let nome = "João"
+    console.log("Parabens, " + nome)
+    console.log("Você pode dirigir")
+}
+console.log("Até mais " + nome)
+
+// const nome = "José"
+// const endereco = `Rua K, 12`
+// const sexo = 'M'
+// const idade = 27
+
+// let a = 2
+// a = "abc"
+
+// var c = 2+3
+// var d = "abcd"
